fd87772aa0afdbf84eb8c55f314c5ddebe5aa88c
NAKATA Maho <maho.nakata@gmail.com>
2024-02-24 18:24:07 +0900

    .

--- examples/example05.cpp
+++ examples/example05.cpp
@@ -5,15 +5,22 @@
 
 int main() {
     // Set initial values
-    mpfr::mpfr_class one(1.0);
+    int decimal_precision = 50;                                                             // You can change this value to adjust the precision
+    int bit_precision = static_cast<int>(std::ceil(decimal_precision * std::log2(10))) * 2; // Calculate bit precision from decimal, * 2 is for sure
+    mpfr::defaults::set_default_prec(bit_precision);
+    std::cout << std::fixed << std::setprecision(decimal_precision); // Set output to fixed-point notation with 50 digits of precision
+
+    int iteration = 0;
+    mpfr::mpfr_class one(1.0); // Second parameter sets the precision in bits
     mpfr::mpfr_class two(2.0);
     mpfr::mpfr_class four(4.0);
     mpfr::mpfr_class a(one), b(one / mpfr::sqrt(two)), t(0.25), p(one);
-    mpfr::mpfr_class a_next, b_next, t_next, pi;
+    mpfr::mpfr_class a_next, b_next, t_next, pi, pi_previous;
+    mpfr::mpfr_class epsilon = mpfr::pow(mpfr::mpfr_class(10.0), mpfr::mpfr_class(-decimal_precision)); // Define the tolerance level for convergence
+    bool converged = false;
 
-    std::cout << std::fixed << std::setprecision(50); // Set output to fixed-point notation with 50 digits of precision
-
-    for (int i = 0; i < 5; ++i) { // Number of iterations
+    while (!converged) {
+        iteration++;
         a_next = (a + b) / two;
         b_next = mpfr::sqrt(a * b);
         t_next = t - p * mpfr::pow(a - a_next, two);
@@ -25,10 +32,18 @@ int main() {
         t = t_next;
 
         // Calculate pi
+        pi_previous = pi;
         pi = mpfr::pow(a + b, two) / (four * t);
 
-        std::cout << "Iteration " << i + 1 << ": π " << pi << std::endl;
+        // Check for convergence
+        if (mpfr::abs(pi - pi_previous) < epsilon) {
+            converged = true;
+        }
+
+        std::cout << "Iteration " << iteration << ": π " << pi << std::endl;
     }
+
+    std::cout << "Converged after " << iteration << " iterations." << std::endl;
 
     return 0;
 }
